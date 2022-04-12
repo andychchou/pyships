78493729fecef4eced4f3df8562ddca3dbfe0b42
Andy Chou <andychchou@gmail.com>
2022-04-12 08:27:52 -0700

    class setup

--- app.py
+++ app.py
@@ -1,56 +1,82 @@
-destroyer = {
-    "hp": 100,
-    "damage": 20,
-    "rof": 80,
-    "accuracy": 30,
-    "range": 30,
-    "speed": 100,
-    "evasion": 80
-}
+class Warship:
+    def __init__(self, hp, damage, rof, accuracy, range, speed, evasion):
+        self.hp = hp
+        self.damage = damage
+        self.rof = rof
+        self.accuracy = accuracy
+        self.range = range
+        self.speed = speed
+        self.evasion = evasion
 
-cruiser = {
-    "hp": 200,
-    "damage": 30,
-    "rof": 50,
-    "accuracy": 30,
-    "range": 50,
-    "speed": 80,
-    "evasion": 60
-}
+    def move_forward(self):
+        print("Moved forward")
 
-battleship = {
-    "hp": 400,
-    "damage": 60,
-    "rof": 20,
-    "accuracy": 30,
-    "range": 70,
-    "speed": 50,
-    "evasion": 30
-}
 
-carrier = {
-    "hp": 100,
-    "damage": 40,
-    "rof": 10,
-    "accuracy": 80,
-    "range": 100,
-    "speed": 80,
-    "evasion": 60
-}
+class Destroyer(Warship):
+    def __init__(self):
+        hp = 100
+        damage = 20
+        rof = 80
+        accuracy = 30
+        range = 30
+        speed = 100
+        evasion = 80
+        super().__init__(hp, damage, rof, accuracy, range, speed, evasion)
+
+
+class Cruiser(Warship):
+    def __init__(self):
+        hp = 200
+        damage = 30
+        rof = 50
+        accuracy = 30
+        range = 50
+        speed = 80
+        evasion = 60
+        super().__init__(hp, damage, rof, accuracy, range, speed, evasion)
+
+
+class Battleship(Warship):
+    def __init__(self):
+        hp = 400
+        damage = 60
+        rof = 20
+        accuracy = 30
+        range = 70
+        speed = 50
+        evasion = 30
+        super().__init__(hp, damage, rof, accuracy, range, speed, evasion)
+
+
+class Carrier(Warship):
+    def __init__(self):
+        hp = 100
+        damage = 40
+        rof = 10
+        accuracy = 80
+        range = 100
+        speed = 80
+        evasion = 60
+        super().__init__(hp, damage, rof, accuracy, range, speed, evasion)
+
+
+ship1 = Destroyer()
+print(ship1.hp)
+ship1.move_forward()
 
 # game setup
-selection = input("Select")
-p1_ship = {}
-p2_ship = {}
+# selection = input("Select")
+# p1_ship = {}
+# p2_ship = {}
 
-while True:
-    if selection == "1":
-        p1_ship = destroyer.copy()
-    elif selection == "2":
-        p1_ship = cruiser.copy()
-    elif selection == "3":
-        p1_ship = battleship.copy()
-    elif selection == "4":
-        p1_ship = cruiser.copy()
+# while True:
+#     if selection == "1":
+#         p1_ship = destroyer.copy()
+#     elif selection == "2":
+#         p1_ship = cruiser.copy()
+#     elif selection == "3":
+#         p1_ship = battleship.copy()
+#     elif selection == "4":
+#         p1_ship = cruiser.copy()
 
-    print("Unknown ship")
+#     print("Unknown ship")
